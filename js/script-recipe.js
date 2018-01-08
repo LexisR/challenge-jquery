6b4205df39ee6b53a409bc583ce3c258eb6e72ef
LexisR <lexisrodriguez22@gmail.com>
2018-01-08 17:30:07 -0300

    Estapa 8 lista

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,8 +3,14 @@ $(document).ready( function(){
   console.log("Funciona");
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
-	renderHighlightedRecipes(recipesArray);
 
+  
 });
- //   $('.slider li:first').show();
-  // $('.pagination li:first').css({'color':'#CD6E2E'});
+ 
+$('.js-show-recipe').on('click', function(){
+  $('.page').removeClass('make');
+});
+
+$('.js-show-make').on('click', function(){
+  $('.page').addClass('make');
+});
